65bb5ae117107d59d853f75a34b56b253c6b7b60
yijkio0 <146135716+yijkio0@users.noreply.github.com>
2024-06-05 04:37:30 +0530

    Add files via upload

---
+++ codingproblem1.py
@@ -0,0 +1,12 @@
+# Question1
+name=str(input("Enter your name: "))
+age=int(input("Enter your age: "))
+print("Hello World!")
+print(name,age,sep='\n')
+# Question 2
+a=int(input("Enter the value of a: "))
+b=int(input("Enter the value of b: "))
+a=a^b
+b=a^b
+a=a^b
+print("a=",a," b=",b)
